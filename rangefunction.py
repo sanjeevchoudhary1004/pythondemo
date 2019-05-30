a84a620aa800de12a509f1fcf423f149eaf535c0
manikantapola <manikantapola@gmail.com>
2019-05-30 12:47:03 +0530

    range function

---
+++ rangefunction.py
@@ -0,0 +1,15 @@
+a = range(10)
+for i in a:
+    print(i)
+
+b = range(10,20)
+for j in b:
+     print(j)
+
+c = range(20,30,2)
+for k in c:
+    print(k)
+
+d = range(50,40,-2)
+for l in d:
+    print(l)
