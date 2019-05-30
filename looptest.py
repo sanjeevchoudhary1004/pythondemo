60bd3e3d04faba15a92996811a97c8ce8b320929
manikantapola <manikantapola@gmail.com>
2019-05-30 11:24:13 +0530

    3RD Commit

--- looptest.py
+++ looptest.py
@@ -6,3 +6,14 @@ if x < 10:
 else:
     print("Given no is  >= 2 digit no")
     print("End")
+
+    print("begin")
+    i = input("Enter a positive no")
+    x = int(i)
+    if x < 10 :
+        print("Given no is 1 digit no")
+    elif x < 100 :
+        print("Given no is 2 digit no")
+    else:
+        print("Given no is  >=4 digit no")
+        print("end")
