34af8bebfc2ce0bee6e702d626e2a1b2226bd67b
manikantapola <manikantapola@gmail.com>
2019-05-30 12:02:53 +0530

    while loop file code Commit

---
+++ whileloop.py
@@ -0,0 +1,8 @@
+while True:
+    name = input("Enter username")
+    if name !='lokesh':
+        continue
+    password = input("Hello Lokesh ! Enter ur Passwpord ?")
+    if password =='python':
+        break
+print('Access Granted')
