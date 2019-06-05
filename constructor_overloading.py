22df28a4f3eb07cfb6cd1c84404233bbf620f19e
manikantapola <manikantapola@gmail.com>
2019-06-05 12:56:49 +0530

    constructor overloading

---
+++ constructor_overloading.py
@@ -0,0 +1,10 @@
+class x:
+    def __init__(self):
+        print("in no param cost of x")
+    def __init__(self,a,b):
+        print("in two param const of x")
+    def __init__(self,a):
+        print("in one param const of x")
+
+x1 =x(1000)
+x2 =x(123 ,456)
